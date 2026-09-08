bc9a44ff12524d7e2125c8f324e348ba403caba7
florianciovirnache-tech <florianciovirnache@gmail.com>
2026-09-08 12:53:52 +0300

    Add sort dropdown test using selectOption and data-test locator

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,11 +5,21 @@ test('saucedomo page', async ({ page }) => {
   await expect(page).toHaveTitle('Swag Labs');
 });
 
+test.describe('Login Saucedemo', () => {
 
-test('fill username', async ({ page }) => { 
-  await page.goto('https://saucedemo.com/');
-  await page.getByPlaceholder('Username').fill('standard_user');
-  await page.getByPlaceholder('Password').fill('secret_sauce');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://saucedemo.com/');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button', { name: 'Login' }).click();
+  });
+    test('fill username', async ({ page }) => {
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  });
+    test('sort products by price',async ({ page }) => {
+    await page.locator('[data-test="product-sort-container"]').selectOption('lohi');
+    await expect(page.locator('[data-test="product-sort-container"]')).toHaveValue('lohi');
 });
+  });
+  
+
